b656c9472a4488b30973cb9143fdaed1427dd2ae
Natálie Vychodilová <vychodilova.natalie@gmail.com>
2022-10-07 13:53:40 +0200

    parsing JSON data

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // constants for creating server
 const http = require('http');
 const {format} = require('date-fns');
+const ws = require("ws"); // websockets
 
+//create local server
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -9,21 +11,21 @@ const port = 3000;
 const WEBSOCKET_HOST = "ws://demo.sewio.net";
 const API_KEY = "17254faec6a60f58458308763";
 
-// LIBS
-const ws = require("ws"); // websockets
 
 // LOGIC
 const socket = new ws(WEBSOCKET_HOST);
+
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World again!');
+    response.end('Hello World!');
     console.log(format(new Date(), 'dd.MM.yyyy\tHH:mm:ss'));
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
 socket.on("open", () => {
     console.log("Connection Established!");
@@ -42,6 +44,12 @@ function sendSubscribeMessage() {
     );
 }
 
-function showData(message){
-    console.log(JSON.parse(message));
+function showData(message) {
+    var x = JSON.parse(message);
+    if(x["body"]["id"] === "22"){
+        var str = "{\"tagID\":\"" + x["body"]["id"] + "\",\"posX\":\"" + x["body"]["datastreams"][0]["current_value"] + "\", \"posY\":\"" + x["body"]["datastreams"][1]["current_value"] + "\"}";
+        console.log(str);
+    }
 }
+
+
